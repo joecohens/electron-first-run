8f71fed7a242344a9ef26cc4e80325cce02dc487
Joe Cohen <joecohenr@gmail.com>
2018-08-11 10:15:26 -0500

    update file name since we now have a directory

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {app} = require('electron');
 const makeDir = require('make-dir');
 
 const getConfigPath = opts => {
-  opts = Object.assign({name: '.electron-app-first-run'}, opts);
+  opts = Object.assign({name: 'electron-app-first-run'}, opts);
 
   const configPath = path.join(app.getPath('userData'), 'FirstRun', opts.name);
 
